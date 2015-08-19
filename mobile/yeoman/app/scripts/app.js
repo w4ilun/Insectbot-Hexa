60333f135ef078ad13a1d82790bdc482cd59d70f
w4ilun <w4ilun@yahoo.com>
2015-08-18 22:53:28 -0400

    generated angular project for scaffolding

---
+++ mobile/yeoman/app/scripts/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name yeomanApp
+ * @description
+ * # yeomanApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('yeomanApp', [
+    'ngTouch'
+  ]);
